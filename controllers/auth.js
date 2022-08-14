7ee07841a86a0db69e4581afa593f16271caa951
CodergirlVS <vaidehis1214@gmail.com>
2022-08-14 11:25:55 -0700

    Password validation and hash is moved to mongoose model Schema

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,25 +1,17 @@
 const User = require("../models/User");
 const { StatusCodes } = require("http-status-codes");
-const {BadRequestError} = require('../errors')
-const bcrypt = require('bcryptjs') // to convert password fom string to hash.
-
+const { BadRequestError } = require("../errors");
+// const bcrypt = require('bcryptjs') // to convert password fom string to hash. This is moved inside the model
+const jwt = require("jsonwebtoken");
 
 const register = async (req, res) => {
-  const {name, email, password} = req.body
-
-//create password hash
-  const salt = await bcrypt.genSalt(10) //random bytes
-  const hashedPassword = await bcrypt.hash(password, salt)
-
-  const tempUser = {name, email, password:hashedPassword}
-
+  //const { name, email, password } = req.body;
 
   // if(!name || !email || !password) {
   //   throw new BadRequestError('Please provide name, email and password')
   //  } // Without the trhow we can still get 500 error taken care by the mongoose validations but we want more meaningful errors
 
-
-  const user = await User.create({ ...tempUser });
+  const user = await User.creage({ ...req.body });
   res.status(StatusCodes.CREATED).json({ user });
 };
 
