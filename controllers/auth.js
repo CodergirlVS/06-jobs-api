8666b965f273664d2eccd43e0fdac23bb6259063
CodergirlVS <vaidehis1214@gmail.com>
2022-08-14 12:54:11 -0700

    Login email is validated but it accepts anything as password

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,6 @@
 const User = require("../models/User");
 const { StatusCodes } = require("http-status-codes");
-const { BadRequestError } = require("../errors");
+const { BadRequestError, UnauthenticatedError } = require("../errors");
 // const bcrypt = require('bcryptjs') // to convert password fom string to hash. This is moved inside the model
 //const jwt = require("jsonwebtoken");
 
@@ -20,7 +20,20 @@ const register = async (req, res) => {
 };
 
 const login = async (req, res) => {
-  res.send("login user");
+const {email, password} = req.body;
+if(!email || !password) {
+  throw new BadRequestError("Please provide email and password")
+}
+
+const user = await User.findOne({email})
+//compare password
+if(!user) {
+  throw new UnauthenticatedError('Invalid Credentials')
+}
+
+const token = user.createJWT();
+
+res.status(StatusCodes.OK).json({user: {name:user.name}, token})
 };
 
 module.exports = { register, login };
