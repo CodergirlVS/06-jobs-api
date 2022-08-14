c7d83777adbb57f1ddb40f71f76c0ab71c816c86
CodergirlVS <vaidehis1214@gmail.com>
2022-08-14 11:47:34 -0700

    Passing the name along with token as response on regiser and including it in a function in schema

--- controllers/auth.js
+++ controllers/auth.js
@@ -11,8 +11,11 @@ const register = async (req, res) => {
   //   throw new BadRequestError('Please provide name, email and password')
   //  } // Without the trhow we can still get 500 error taken care by the mongoose validations but we want more meaningful errors
 
-  const user = await User.creage({ ...req.body });
-  res.status(StatusCodes.CREATED).json({ user });
+  const user = await User.create({ ...req.body });
+  const token = jwt.sign({userId: user._id, name:user.name}, 'jwtSecret', {
+    expiresIn: '30d',
+  })
+  res.status(StatusCodes.CREATED).json({ name: user.getName(), token });
 };
 
 const login = async (req, res) => {
--- models/User.js
+++ models/User.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-const bcrypt = require('bcryptjs') 
+const bcrypt = require("bcryptjs");
 
 const UserSchema = new mongoose.Schema({
   name: {
@@ -11,23 +11,28 @@ const UserSchema = new mongoose.Schema({
   email: {
     type: String,
     required: [true, "Please provide email"], // field is required and there is error message
-    match: [ //match with regex
+    match: [
+      //match with regex
       /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
       "Please provide a valid email",
     ],
     unique: true, //creates a unique index
   },
-  password:{
+  password: {
     type: String,
     required: [true, "Please provide password"],
     minlength: 6,
   },
 });
 
-UserSchema.pre('save', async function() {
+UserSchema.pre("save", async function () {
   //create password hash
-  const salt = await bcrypt.genSalt(10)
-  this.password = await bcrypt.hash(this.password, salt)
-} )
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+});
 
-module.exports = mongoose.model('User', UserSchema);
+UserSchema.methods.getName = function() {
+  return this.name;
+}
+
+module.exports = mongoose.model("User", UserSchema);
