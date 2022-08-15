7b70b1e07f31faa519905a361b615feec2041272
CodergirlVS <vaidehis1214@gmail.com>
2022-08-15 10:55:07 -0700

    Updated syntax

--- app.js
+++ app.js
@@ -2,10 +2,10 @@ require("dotenv").config();
 require("express-async-errors");
 
 //extra security packages
-const helmet = requrie("helmet");
-const cros = require("cors");
-const xss = require("xss-clean");
-const rateLimiter = requrie('express-rate-limit')
+const helmet = require('helmet');
+const cors = require('cors');
+const xss = require('xss-clean');
+const rateLimiter = require('express-rate-limit');
 
 const express = require("express");
 const app = express();
